e617ea268e3e22ccb0be77b45e8fba3c28513df2
Tom Hennessy <tomlhennessy@outlook.com>
2024-08-25 12:19:22 +0100

    phase 1 complete

--- social.js
+++ social.js
@@ -2,29 +2,45 @@
 class SocialNetwork {
 
   constructor() {
-    this.users = {};
-    this.follows = {};
-    this.currentID = 0;
+    this.users = {};    // to store user objects { id: 1, name: `John Doe` }
+    this.follows = {};  // to store follow relationships { 1: Set {2, 3}}
+    this.currentID = 0; // to assign unique IDs incrementally
   }
 
   addUser(name) {
-    // Your code here
+    this.currentID += 1; // increment ID
+    const userID = this.currentID;
+    this.users[userID] = { id: userID, name }; // store user object
+    this.follows[userID] = new Set(); // initialise an empty set for follows
+    return userID;
   }
 
   getUser(userID) {
-    // Your code here
+    return this.users[userID] || null;
   }
 
   follow(userID1, userID2) {
-    // Your code here
+    if (this.users[userID1] && this.users[userID2]) {
+      this.follows[userID1].add(userID2); // add userID2 to the follow set of userID1
+      return true;
+    }
+    return false;
   }
 
   getFollows(userID) {
-    // Your code here
+    return this.follows[userID] || new Set();
   }
 
   getFollowers(userID) {
-    // Your code here
+    const followers = new Set();
+
+    for (let followerID in this.follows) {
+      if (this.follows[followerID].has(userID)) {
+        followers.add(parseInt(followerID));
+      }
+    }
+
+    return followers;
   }
 
   getRecommendedFollows(userID, degrees) {
